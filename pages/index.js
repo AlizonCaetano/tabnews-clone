2a871080e32a983302caf2266811706b0066f475
AlizonCaetano <alison.natan17@hotmail.com>
2023-09-02 19:32:55 -0300

    adiciona lib styled-components, cria de construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box; 
+
+  display: grid;
+  height: 100vh;
+  width: 100%;
+  place-content: center;
+
+  color: #fff;
+  background: #222;
+`
+
 function Home() {
-  return <h1>Hello, tabnews!</h1>;
+  return (
+    <Container>
+      <div>
+        EM CONSTRUÇÃO
+      </div>  
+    </Container>
+  )
 }
 
 export default Home;
